cff3f41b4d14fa2b85d6b26a130beaae74dccccd
opilane <opilane@HP-CZC3217069>
2025-09-04 15:00:59 +0300

    a

---
+++ TallinnaRakenduslikKolledz/Data/DbInitializer.cs
@@ -0,0 +1,26 @@
+﻿using TallinnaRakenduslikKolledz.Models;
+
+namespace TallinnaRakenduslikKolledz.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(SchoolContext context)
+        {
+            context.Database.EnsureCreated();
+            if (context.Students.Any())
+            {
+                return;
+            }
+
+            var students = new Student[]
+            {
+                new Student
+                {
+                    FirstName = "George",
+                    LastName = "Teemus",
+                    EnrollmentDate = DateTime.Now,
+                }
+            };
+        }
+    }
+}
--- TallinnaRakenduslikKolledz/Program.cs
+++ TallinnaRakenduslikKolledz/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using TallinnaRakenduslikKolledz.Data;
+
 namespace TallinnaRakenduslikKolledz
 {
     public class Program
@@ -8,6 +11,9 @@ namespace TallinnaRakenduslikKolledz
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<SchoolContext>(options => options.UseSqlServer
+                (builder.Configuration.GetConnectionString("DefaultConnection")));
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             var app = builder.Build();
 
