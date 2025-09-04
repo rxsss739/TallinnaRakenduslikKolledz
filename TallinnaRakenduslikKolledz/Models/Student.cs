346f27b73f945d276e3ad07508f308bb7d3304c1
opilane <opilane@HP-CZC3217069>
2025-09-04 12:44:35 +0300

    Add project files.

---
+++ TallinnaRakenduslikKolledz/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TallinnaRakenduslikKolledz.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public ICollection<Enrollment> Enrollments { get; set; }
+        public int? Age { get; set; }
+        public string? Group { get; set; }
+        public string? Gender { get; set; }
+    }
+}
