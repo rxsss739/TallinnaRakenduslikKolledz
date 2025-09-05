a8a7b883321d2325517a204ee955edc49ded4e05
opilane <opilane@HP-CZC321705Z>
2025-09-05 11:37:29 +0300

    a

---
+++ TallinnaRakenduslikKolledz/Controllers/StudentsController.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TallinnaRakenduslikKolledz.Data;
+
+namespace TallinnaRakenduslikKolledz.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly SchoolContext _context;
+        public StudentsController(SchoolContext context) 
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Students.ToListAsync());
+        }
+    }
+}
