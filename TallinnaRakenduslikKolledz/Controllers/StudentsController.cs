7170b0d84e43e360d1a3bd76912364febceb8325
opilane <opilane@HP-CZC321705Z>
2025-09-10 13:15:53 +0300

    a

--- TallinnaRakenduslikKolledz/Controllers/StudentsController.cs
+++ TallinnaRakenduslikKolledz/Controllers/StudentsController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
 using Microsoft.EntityFrameworkCore;
 using TallinnaRakenduslikKolledz.Data;
 using TallinnaRakenduslikKolledz.Models;
@@ -38,6 +39,31 @@ namespace TallinnaRakenduslikKolledz.Controllers
             }
 
             return View(student);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var student = await _context.Students.FirstOrDefaultAsync(m => m.Id == id);
+            
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var student = await _context.Students.FindAsync(id);
+            _context.Students.Remove(student);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
     }
 }
