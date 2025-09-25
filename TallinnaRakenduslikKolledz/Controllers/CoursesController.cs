f40b5430e2a24eeb89ef444464d49a6fc2c087c0
opilane <opilane@HP-CZC3217069>
2025-09-25 16:36:15 +0300

    a

---
+++ TallinnaRakenduslikKolledz/Controllers/CoursesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TallinnaRakenduslikKolledz.Data;
+
+namespace TallinnaRakenduslikKolledz.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly SchoolContext _context;
+        public CoursesController(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var courses = _context.Courses.Include(c => c.Department)
+                .AsNoTracking();
+
+            return View(courses);
+        }
+
+        [HttpGet]
+        public IActionResult Create(int id)
+        {
+            PopulateDepartmentsDropDownList();
+            return View();
+        }
+
+        private void PopulateDepartmentsDropDownList(object selectedDepartment = null)
+        {
+            var departmentsQuery = from d in _context.Departments
+                                   orderby d.Name
+                                   select d;
+            ViewBag.DepartmentID = new SelectList(departmentsQuery.AsNoTracking(), "DepartmentID", "Name", selectedDepartment);
+        }
+    }
+}
--- TallinnaRakenduslikKolledz/Models/Course.cs
+++ TallinnaRakenduslikKolledz/Models/Course.cs
@@ -6,5 +6,8 @@
         public string Title { get; set; }
         public int Credits { get; set; }
         public ICollection<Enrollment> Enrollments { get; set; }
+        public Department? Department { get; set; }
+        public int? DepartmentID { get; set; }
+        public ICollection<CourseAssignment>? CourseAssignments { get; set; }
     }
 }
